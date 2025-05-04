98776316130388a4faffa1b9dd96bc8feb9729d1
dynaxz <smoothxeliturhan@gmail.com>
2025-05-04 17:44:37 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local placeId = game.PlaceId
+ 
+if placeId == 71895508397153 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/dieofdeath.lua"))()
+elseif placeId == 18687417158 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsaken.lua"))()
+else
+    warn("no script configured for this game.")
+end
