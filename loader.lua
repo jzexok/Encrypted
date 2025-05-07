17d0246043a3d49d0ba27dc45c06b6a68f0f65a0
dynaxz <smoothxeliturhan@gmail.com>
2025-05-07 06:02:45 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,6 +6,8 @@ elseif placeId == 18687417158 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsaken.lua"))()
 elseif placeId == 79834168261692 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsakenbutbad.lua"))()
+elseif placeId == 17164617414 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/fpes.lua"))()
 else
     warn("no script configured for this game.")
 end
