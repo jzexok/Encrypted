dc1ef003fea0ed74d68712937f7eac0f7cb507ec
dynaxz <smoothxeliturhan@gmail.com>
2025-05-07 12:37:52 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,8 +2,6 @@ local placeId = game.PlaceId
  
 if placeId == 71895508397153 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/dieofdeath.lua"))()
-elseif placeId == 18687417158 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsaken.lua"))()
 elseif placeId == 79834168261692 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsakenbutbad.lua"))()
 elseif placeId == 17164617414 then
