28ea094671a82751420036146b3a8ede12021292
63.dll <smoothxeliturhan@gmail.com>
2025-05-20 08:33:28 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,15 +1,15 @@
 local placeId = game.PlaceId
  
 if placeId == 71895508397153 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/dieofdeath.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/dieofdeath.lua"))()
 elseif placeId == 79834168261692 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsakenbutbad.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsakenbutbad.lua"))()
 elseif placeId == 17164617414 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/fpes.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fpes.lua"))()
 elseif placeId == 74161468227974 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/fivenightshunted.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"))()
 elseif placeId == 18687417158 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dynaxz/purna.xyz/refs/heads/main/forsaken.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsaken.lua"))()
 else
     warn("no script configured for this game.")
 end
