af962e150b1f1b2d0384b2d962b459d0b99dc146
63.dll <smoothxeliturhan@gmail.com>
2025-05-21 16:00:30 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,7 @@ elseif placeId == 79834168261692 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsakenbutbad.lua"))()
 elseif placeId == 17164617414 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fpes.lua"))()
-elseif placeId == 74161468227974 then
+elseif placeId == 6858589072 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"))()
 elseif placeId == 18687417158 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsaken.lua"))()
