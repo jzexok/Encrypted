e1c80790d558af108ec77c2972db2d63edc00c0e
63.dll <smoothxeliturhan@gmail.com>
2025-05-21 16:10:08 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,15 +1,15 @@
-local placeId = game.PlaceId
- 
-if placeId == 71895508397153 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/dieofdeath.lua"))()
-elseif placeId == 79834168261692 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsakenbutbad.lua"))()
-elseif placeId == 17164617414 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fpes.lua"))()
-elseif placeId == 6858589072 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"))()
-elseif placeId == 18687417158 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsaken.lua"))()
+local a = game.PlaceId
+
+local b = {
+    [71895508397153] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/dieofdeath.lua",
+    [79834168261692] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsakenbutbad.lua",
+    [17164617414]    = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fpes.lua",
+    [18687417158]    = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsaken.lua",
+    [74161468227974] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"
+}
+
+if b[a] then
+    loadstring(game:HttpGet(b[a]))()
 else
-    warn("no script configured for this game.")
+    warn("script doesnt exist for this game.")
 end
