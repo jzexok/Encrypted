1471a948f54f82847b6262d72385dbd6fbe5922c
jzexok <smoothxeliturhan@gmail.com>
2025-05-30 20:26:25 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,7 @@ local b = {
     [79834168261692] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsakenbutbad.lua",
     [17164617414]    = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fpes.lua",
     [18687417158]    = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/forsaken.lua",
-    [74161468227974] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"
+    [74161468227974] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/fivenightshunted.lua"-
     [121418861436763] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/inamegastore.lua",
     [2413927524] = "https://raw.githubusercontent.com/63dl/Encrypted/refs/heads/main/therakeremastered.lua"
 }
